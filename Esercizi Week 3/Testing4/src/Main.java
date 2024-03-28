53d89fbe95f8acca64a1a1629a7597d1b2e4ca00
marcoferraro49 <160497711+marcoferraro49@users.noreply.github.com>
2024-03-28 14:10:43 +0100

    modifica esercizio testing 4

--- Esercizi Week 3/Testing4/src/Main.java
+++ Esercizi Week 3/Testing4/src/Main.java
@@ -5,13 +5,20 @@ public class Main {
     public static void main(String[] args) {
 
         OffsetDateTime dateTime = OffsetDateTime.parse("2023-03-01T13:00:00Z");
-        
-        int year = dateTime.getYear();
-        Month month = dateTime.getMonth();
-        int day = dateTime.getDayOfMonth();
+        System.out.println(getYear(dateTime));
+        System.out.println(getMonth(dateTime));
+        System.out.println(getDay(dateTime));
+    }
 
-        System.out.println(year);
-        System.out.println(month);
-        System.out.println(day);
+    public static int getYear(OffsetDateTime date){
+        return date.getYear();
+    }
+
+    public static Month getMonth (OffsetDateTime date){
+        return date.getMonth();
+    }
+
+    public static int getDay(OffsetDateTime date){
+        return date.getDayOfMonth();
     }
 }
--- Esercizi Week 3/Testing4/src/MainTest.java
+++ Esercizi Week 3/Testing4/src/MainTest.java
@@ -1,27 +1,30 @@
+import org.junit.jupiter.api.Test;
+
 import java.time.Month;
 import java.time.OffsetDateTime;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
-    @org.junit.jupiter.api.Test
-    void testGetYear(){
-        OffsetDateTime dateTime = OffsetDateTime.parse("2023-03-01T13:00:00Z");
-        int year = dateTime.getYear();
+
+    @Test
+    void getYear() {
+        OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        int year = date.getYear();
         assertEquals(year, 2023);
     }
 
-    @org.junit.jupiter.api.Test
-    void testGetMonth(){
-        OffsetDateTime dateTime = OffsetDateTime.parse("2023-03-01T13:00:00Z");
-        Month month = dateTime.getMonth();
+    @Test
+    void getMonth() {
+        OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        Month month = date.getMonth();
         assertEquals(month, Month.MARCH);
     }
 
-    @org.junit.jupiter.api.Test
-    void testGetDayOfMonth() {
-        OffsetDateTime dateTime = OffsetDateTime.parse("2023-03-01T13:00:00Z");
-        int day = dateTime.getDayOfMonth();
+    @Test
+    void getDay() {
+        OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        int day = date.getDayOfMonth();
         assertEquals(day, 1);
     }
 }
